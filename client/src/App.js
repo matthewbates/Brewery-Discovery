fd41c534cd11297272fcf5667492c6f632b7a6fe
Myles <bracken.myles@gmail.com>
2022-03-05 12:18:33 -0800

    updated navbar styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Reviews from "./components/Reviews";
@@ -11,18 +11,31 @@ function App() {
       <div className="App">
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand as={Link} to="/">Brewery Discovery</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/reviews">
-                Reviews
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-            </Nav>
+            <Navbar.Brand as={Link} to="/">
+              Brewery Discovery
+            </Navbar.Brand>
+            <div className="d-flex justify-content-end align-items-center">
+              <Nav className="me-auto">
+                <Nav.Link className="mx-2" as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link className="mx-2" as={Link} to="/reviews">
+                  Reviews
+                </Nav.Link>
+                {/* <Nav.Link as={Link} to="/login"> */}
+                <div>
+                  <Button
+                    className="mx-2 nav-login-btn"
+                    as={Link}
+                    to="/login"
+                    variant="outline-light"
+                  >
+                    Login
+                  </Button>
+                </div>
+                {/* </Nav.Link> */}
+              </Nav>
+            </div>
           </Container>
         </Navbar>
         <div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
 
 function Home() {
-    return (
-        <>
-        <h2>Welcome To The Brewery Discovery Project</h2>
-        </>
-    )
+  return (
+    <>
+      <h3 className="p-2">Welcome To The Brewery Discovery Project</h3>
+      <Container>
+        <Row>
+          <Col xs={12} lg={6}>1 of 2</Col>
+          <Col xs={12} lg={6}>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Home
+export default Home;
