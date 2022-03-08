749720fc505b8ad68221eeecf711618f66947a34
Matthew <matthewhcbates@gmail.com>
2022-03-08 16:37:09 -0700

    added state filter

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, DropdownButton, Dropdown } from "react-bootstrap";
 
 function Search({ brewerySearch, brewerySetter, stateSearch, stateSetter }) {
   function renderBrewerySearch(e) {
     brewerySetter(e.target.value);
   }
 
-  function renderStateSearch(e) {
-    stateSetter(e.target.value);
+  function handleSelect(e) {
+    stateSetter(e);
   }
 
   return (
@@ -23,16 +23,14 @@ function Search({ brewerySearch, brewerySetter, stateSearch, stateSetter }) {
           onChange={renderBrewerySearch}
         />
 
-        <input
-          type="text"
-          className="form-control rounded"
-          aria-label="Search"
-          aria-describedby="search-addon"
-          value={stateSearch}
-          placeholder="Search By State"
-          onChange={renderStateSearch}
-        />
-        <Button className="search_by_brewery_or_state">Search</Button>
+        <DropdownButton
+          id="dropdown-basic-button"
+          title="Search By State"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item eventKey="CO">CO</Dropdown.Item>
+          <Dropdown.Item eventKey="CA">CA</Dropdown.Item>
+        </DropdownButton>
       </div>
     </Container>
   );
