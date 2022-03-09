60119be090a152bcc78e50f98d9487f90387bff0
Myles <bracken.myles@gmail.com>
2022-03-08 18:13:12 -0800

    updated brewery cards with logos and text

--- client/src/components/BreweryCard.js
+++ client/src/components/BreweryCard.js
@@ -16,7 +16,7 @@ function BreweryCard({
     <>
       <Col sm={12} md={12} lg={6} className="gap-3 mb-5 mt-5">
         <Card className="card-div mx-auto">
-          <Card.Img className="img-responsive" src={image} alt="" />
+          <Card.Img className="img-responsive card-img-filter" src={image} alt="" />
           <Card.ImgOverlay className="card-img-o">
             <div className="p-4 mt-2 flex-column d-flex justify-content-center text-light">
               <Card.Title className="update_title_color">
@@ -30,7 +30,7 @@ function BreweryCard({
               <Card.Text className="card-p-text p-1">{phone}</Card.Text>
             </div>
 
-            <div className="card-footer-div bg-light text-muted ">
+            <div className="card-footer-div bg-light text-muted text-center ">
               <div className="card-b-logo bg-white mx-auto">
                 <Image className="" src={logo} alt="" />
               </div>
